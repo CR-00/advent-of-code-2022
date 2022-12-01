79a3a852a1dfceb90a02febc5bd0c2eaddad804a
CR <CJR022@student.bham.ac.uk>
2022-12-01 21:18:33 +0000

    Day one solution.

---
+++ 01.py
@@ -0,0 +1,20 @@
+import operator
+from pprint import pp
+
+data = []
+
+with open("./inputs/01-part-one.txt", "r") as f:
+    for line in f.readlines():
+        data += [line.strip("\n")]
+
+
+def part_one():
+    return max(enumerate([sum(list(map(int, j))) for j in [i.split(",") for i in ",".join(data).split(",,")]]), key=operator.itemgetter(1))[1]
+
+def part_two():
+    return sum(sorted([sum(list(map(int, j))) for j in [i.split(",") for i in ",".join(data).split(",,")]], reverse=True)[:3])
+
+
+if __name__ == "__main__":
+    print(f"Part One: {part_one()}")
+    print(f"Part One: {part_two()}")
